d8f37457fb6e28164ea0388021595d2e8b2cc33d
LanDev01 <alnjr0001@outlook.com>
2024-05-23 18:25:16 -0300

    chore: migration initial

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,11 +5,14 @@ namespace ApiCrud.Data
 {
     public class AppDbContext : DbContext
     {
-        private DbSet<Estudante> Estudantes { get; set; }
+        public DbSet<Estudante> Estudantes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(connectionString: "Data Source=Banco.sqlite");
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+            optionsBuilder.EnableSensitiveDataLogging(); // Deixar somente em ambiente de desenvolvimento
+
             base.OnConfiguring(optionsBuilder);
         }
     }
---
+++ Migrations/20240523200349_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCrud.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estudantes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Ativo = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudantes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estudantes");
+        }
+    }
+}

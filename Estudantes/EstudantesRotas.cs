a7754af8890e744347ea2f229bf6e4621fdb35d5
LanDev01 <alnjr0001@outlook.com>
2024-05-23 16:25:32 -0300

    Feat: Configurando Rotas e adicionando nova class Estudantes

---
+++ Estudantes/EstudantesRotas.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiCrud.Estudantes
+{
+    public static class EstudantesRotas
+    {
+        public static void AddRotasEstudantes(this WebApplication app)
+        {
+            app.MapGet("estudantes", handler:() => "Hello Estudantes");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using ApiCrud.Estudantes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,5 +17,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Configurando Rotas
+app.AddRotasEstudantes();
 
 app.Run();
